1479d9762007d2fedbc6b017722d919ea6117dd0
Eyneya <eyneya@gmail.com>
2026-07-11 23:52:13 -0400

    Updated supabase.ts

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -2,11 +2,11 @@ import { createClient } from '@supabase/supabase-js';
 
 const SUPABASE_URL =
   import.meta.env.VITE_SUPABASE_URL ||
-  'https://0ec90b57d6e95fcbda19832f.supabase.co';
+  'https://ammcejsokaivxalfhyan.supabase.co';
 
 const SUPABASE_ANON_KEY =
   import.meta.env.VITE_SUPABASE_ANON_KEY ||
-  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJib2x0IiwicmVmIjoiMGVjOTBiNTdkNmU5NWZjYmRhMTk4MzJmIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTg4ODE1NzQsImV4cCI6MTc1ODg4MTU3NH0.9I8-U0x86Ak8t2DGaIk0HfvTSLsAyzdnz-Nw00mMkKw';
+  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImFtbWNlanNva2FpdnhhbGZoeWFuIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODM4MTgwMjQsImV4cCI6MjA5OTM5NDAyNH0.mHAs_jTF8YbPMzm4weRwDnQvr_zoNNad7U6Unf0135Q';
 
 if (!import.meta.env.VITE_SUPABASE_URL) {
   console.warn(
